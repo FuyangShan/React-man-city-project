2d51bdde6fce73694b2c5ca1063a9b6ee6d61731
Shan Fuyang <fuyang@Shans-MBP.attlocal.net>
2019-09-25 20:24:57 -0700

    Creat home component and structure for animation and info

--- src/Components/Header_footer/Header.js
+++ src/Components/Header_footer/Header.js
@@ -5,6 +5,8 @@ import Button from "@material-ui/core/Button";
 
 import { Link } from "react-router-dom";
 
+import { CityLogo } from "../ui/icons";
+
 class Header extends Component {
   render() {
     return (
@@ -18,12 +20,14 @@ class Header extends Component {
         }}
       >
         <Toolbar style={{ display: "flex" }}>
-          <div styles={{ felxGrow: 1 }}>
-            <div className="header_logo">LOGO</div>
+          <div style={{ flexGrow: 1 }}>
+            <div className="header_logo">
+              <CityLogo link={true} linkTo="/" width="70px" height="70px" />
+            </div>
           </div>
 
           <Link to="/the_team">
-            <Button color="inherit">The Team</Button>
+            <Button color="inherit">The team</Button>
           </Link>
           <Link to="/the_matches">
             <Button color="inherit">Matches</Button>
---
+++ src/Components/home/featured/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Stripes from "./Stripes";
+import Text from "./Text";
+
+const Featured = () => {
+  return <div className="featured_wrapper"></div>;
+};
+
+export default Featured;
